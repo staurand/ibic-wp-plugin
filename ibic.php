382952bb77895c5d660c9598f7822da070a0e354
Stephane Taurand <staurand@tsjm.fr>
2022-02-17 08:51:14 +0100

    Fix whitespaces

--- ibic.php
+++ ibic.php
@@ -7,8 +7,8 @@
  * Text Domain:     ibic
  * Domain Path:     /languages
  * Version:         1.0.0
- * License:			GPLv2 or later
- * License URI:		https://www.gnu.org/licenses/gpl-2.0.html
+ * License:         GPLv2 or later
+ * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
  *
  * @package         Ibic
  */
