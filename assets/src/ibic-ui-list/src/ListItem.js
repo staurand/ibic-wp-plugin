0543fb47f2d022345f80967e089d159b4680a077
Stephane Taurand <staurand@tsjm.fr>
2022-03-09 21:30:59 +0100

    Fix key warning

--- assets/src/ibic-ui-list/src/ListItem.js
+++ assets/src/ibic-ui-list/src/ListItem.js
@@ -12,7 +12,7 @@ function ListItem({ error, errors, state, id, name, thumbnail, retryHandler }) {
 		<img className="ibic-list__item__thumbnail" src={ thumbnail } alt="" />
 		<span className="ibic-list__item__name">{ name }</span>
 		{ hasError ? <span className="ibic-list__item__error">{
-			errors ? errors.map(error => <Translate string={ error } />) : error
+			errors ? errors.map((error, index) => <Translate key={ index } string={ error } />) : error
 		}</span> : null }
 		<button className="button" onClick={ retryHandler(id) }>
 			<span className="dashicons-before dashicons-controls-repeat"></span>
