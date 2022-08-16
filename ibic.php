39a86b9a4ad8580dbc85621b62c4201232ef531d
Stephane Taurand <staurand@tsjm.fr>
2022-08-16 18:06:13 +0200

    Fix plugin version

--- ibic.php
+++ ibic.php
@@ -6,7 +6,7 @@
  * Author:          Stéphane Taurand
  * Text Domain:     ibic
  * Domain Path:     /languages
- * Version:         1.0.1
+ * Version:         1.1.0
  * License:         GPLv2 or later
  * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
  *
